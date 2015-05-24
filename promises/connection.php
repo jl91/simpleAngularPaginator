d518ae99afbcadd7a26a0480a7f8e9226d03eb33
= <=>
2015-05-24 15:18:21 -0300

    preparedStatement

--- promises/connection.php
+++ promises/connection.php
@@ -7,17 +7,20 @@ $pdo->exec('SET CHARACTER SET UTF-8');
 
 $query = ' SELECT * FROM usuario ';
 
+$params = [];
 if($request->limit){
-   $query .= " limit {$request->limit} " ;
+   $query .= " limit :limit " ;
+   $params['limit'] = (int) $request->limit;
 }
 
 if($request->offset){
-   $query .= " offset {$request->offset} " ;
+   $query .= " offset :offset " ;
+   $params['offset'] = (int) $request->offset;
 }
 
 
 $stmt = $pdo->prepare($query);
-$stmt->execute();
+$stmt->execute($params);
 $usuarios = $stmt->fetchAll();
 
 $stmt = $pdo->prepare('select count(*) as total from usuario');
